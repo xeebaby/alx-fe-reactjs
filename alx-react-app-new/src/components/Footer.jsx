bc90a9d9f65971cebee59fbea1bc52b5d03ae5a5
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-07-22 21:05:19 +0000

    Styled components with inline CSS

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer.jsx
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', padding: '10px', textAlign: 'center' }}>
+      <p>© 2025 My React App</p>
+    </footer>
+  );
+};
+
+export default Footer;
