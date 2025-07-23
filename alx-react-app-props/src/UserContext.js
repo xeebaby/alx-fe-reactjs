443f78d8952b9ae7b46d6ecb0ae4581e64bc91ff
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-07-23 14:59:52 +0000

    Implemeted Context API across App, UserProfile and UserContext

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,4 @@
+// src/UserContext.js
+import { createContext } from 'react';
+
+export const UserContext = createContext();
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
+
+function UserProfile() {
+  const { name, age, bio } = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
