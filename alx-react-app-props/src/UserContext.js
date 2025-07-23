70f36da93efa1c174113a70bb28ee4812df145a3
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-07-23 15:28:38 +0000

    Refactored prop drilling to use Content API

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,4 +1,17 @@
-// src/UserContext.js
-import { createContext } from 'react';
+// alx-react-app-props/src/UserContext.js
+import { createContext, useState } from 'react';
 
 export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userData] = useState({
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  });
+
+  return (
+    <UserContext.Provider value={userData}>
+      {children}
+    </UserContext.Provider>
+  );
+};
