805f2ae532a0d6610e14b8ae27934def79273568
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-07-23 16:45:33 +0000

    added css inline styling to navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
-      <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-      <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
-      <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
-      <Link to="/contact">Contact</Link>
+    <nav
+      style={{
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'space-around',
+        padding: '10px'
+      }}
+    >
+      <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
