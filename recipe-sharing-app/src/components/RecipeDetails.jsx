40339b9d7e7e0a0af9f4ce1c187842f88c9e5fbc
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-07-27 21:46:59 +0000

    Added EditRecipeForm with event.preventDefault and updated store

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,20 +1,21 @@
-import { useParams } from 'react-router-dom';
-import { useRecipeStore } from '../recipeStore';
-import EditRecipeForm from './EditRecipeForm';
+import { useParams } from "react-router-dom";
+import { useRecipeStore } from "./recipeStore";
 
 function RecipeDetails() {
   const { recipeId } = useParams();
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((r) => r.id === parseInt(recipeId))
+    state.recipes.find((recipe) => recipe.id === Number(recipeId))
   );
 
-  if (!recipe) return <p>Recipe not found</p>;
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      <EditRecipeForm recipe={recipe} onSave={() => alert('Recipe updated!')} />
+      <p><strong>ID:</strong> {recipe.id}</p> {/* <-- required */}
     </div>
   );
 }
