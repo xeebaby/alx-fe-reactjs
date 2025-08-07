6fe8073c7f578ab60258eafdbcddefa85c644b72
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-08-07 14:47:42 +0000

    Added a user serach

--- github-user-search/src/services/githubApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-};
