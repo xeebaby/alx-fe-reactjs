41cc36b03efc39c4ab9ce912f78784cc546cb4e0
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-08-14 14:45:41 +0000

    Building the Recipe Detail Pagees and layout adjustments

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
