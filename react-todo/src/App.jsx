15dc4886d9a6f1081839948972457daf8e5af9d6
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-08-28 17:57:38 +0000

    Implementing and Testing a Todo List Component in React

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div>
+      <h1>React Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
