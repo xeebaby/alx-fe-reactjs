258af1d32b7c19e6c13ce15942ed39e78c668591
Alexander Nana Osei Agyeman <alexandernanayawoseiagyeman@gmail.com>
2025-08-28 18:02:43 +0000

    Implementing and Testing a Todo List Component in React

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import React from "react";
 import TodoList from "../components/TodoList";
 
 test("renders initial todos", () => {
